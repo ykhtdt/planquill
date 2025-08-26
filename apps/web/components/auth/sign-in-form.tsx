ad0468e8e28eaf291936c4d57087146a2f89f5a0
jgpark-1 <jgpark7281@gmail.com>
2025-08-27 04:08:02 +0900

    feat: SignInForm 기본 UI 추가

---
+++ apps/web/components/auth/sign-in-form.tsx
@@ -0,0 +1,80 @@
+import { cn } from "@workspace/ui/lib/utils"
+import { Button } from "@workspace/ui/components/button"
+import { Input } from "@workspace/ui/components/input"
+import { Label } from "@workspace/ui/components/label"
+import {
+  Card,
+  CardContent,
+} from "@workspace/ui/components/card"
+
+import { GithubIcon } from "@/components/icons/github-icon"
+import { GithubSigninButton } from "@/components/buttons/github-signin-button"
+
+type SignInFormProps = React.ComponentProps<"div">
+
+export const SignInForm = ({
+  className,
+  ...props
+}: SignInFormProps) => {
+  return (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="py-0 overflow-hidden">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome</h1>
+                <p className="text-sm text-balance text-muted-foreground">Please sign in to continue</p>
+              </div>
+              <div className="grid gap-2">
+                <Label htmlFor="email">Email</Label>
+                <Input id="email" type="email" placeholder="Enter your email address" required />
+              </div>
+              <div className="grid gap-2">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a href="#" className="ml-auto text-sm underline-offset-2 hover:underline">
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input id="password" type="password" placeholder="Enter your password" required />
+              </div>
+              <Button type="submit" className="w-full">
+                Login
+              </Button>
+              <div className="relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t after:border-border">
+                <span className="relative z-10 bg-background px-2 text-muted-foreground">Or</span>
+              </div>
+              <div className="grid grid-cols-2 gap-4">
+                <GithubSigninButton>
+                  <GithubIcon className="w-4 h-4" />
+                </GithubSigninButton>
+                <Button variant="outline" className="w-full">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
+                    <path
+                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
+                      fill="currentColor"
+                    />
+                  </svg>
+                  <span className="sr-only">Login with Google</span>
+                </Button>
+              </div>
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <a href="#" className="underline underline-offset-4">
+                  Sign up
+                </a>
+              </div>
+            </div>
+          </form>
+          <div className="relative hidden md:flex items-center justify-center border-l">
+
+          </div>
+        </CardContent>
+      </Card>
+      <div className="text-balance text-center text-xs text-muted-foreground [&_a]:underline [&_a]:underline-offset-4 hover:[&_a]:text-primary">
+        By clicking continue, you agree to our <a href="#">Terms of Service</a> • <a href="#">Privacy Policy</a>.
+      </div>
+    </div>
+  )
+}
