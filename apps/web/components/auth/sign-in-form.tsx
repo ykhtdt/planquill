fc39e3771101ff17be89628c54de5fb7bc22550d
Jonggwang Park <jgpark7281@gmail.com>
2025-08-27 17:05:06 +0900

    feat: GoogleSignInButton을 SignInForm에 적용

--- apps/web/components/auth/sign-in-form.tsx
+++ apps/web/components/auth/sign-in-form.tsx
@@ -8,7 +8,9 @@ import {
 } from "@workspace/ui/components/card"
 
 import { GithubIcon } from "@/components/icons/github-icon"
+import { GoogleIcon } from "@/components/icons/google-icon"
 import { GithubSignInButton } from "@/components/buttons/github-sign-in-button"
+import { GoogleSignInButton } from "@/components/buttons/google-sign-in-button"
 
 type SignInFormProps = React.ComponentProps<"div">
 
@@ -49,15 +51,9 @@ export const SignInForm = ({
                 <GithubSignInButton>
                   <GithubIcon className="w-4 h-4" />
                 </GithubSignInButton>
-                <Button variant="outline" className="w-full">
-                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
-                    <path
-                      d="M12.48 10.92v3.28h7.84c-.24 1.84-.853 3.187-1.787 4.133-1.147 1.147-2.933 2.4-6.053 2.4-4.827 0-8.6-3.893-8.6-8.72s3.773-8.72 8.6-8.72c2.6 0 4.507 1.027 5.907 2.347l2.307-2.307C18.747 1.44 16.133 0 12.48 0 5.867 0 .307 5.387.307 12s5.56 12 12.173 12c3.573 0 6.267-1.173 8.373-3.36 2.16-2.16 2.84-5.213 2.84-7.667 0-.76-.053-1.467-.173-2.053H12.48z"
-                      fill="currentColor"
-                    />
-                  </svg>
-                  <span className="sr-only">Login with Google</span>
-                </Button>
+                <GoogleSignInButton>
+                  <GoogleIcon className="w-4 h-4" />
+                </GoogleSignInButton>
               </div>
               <div className="text-center text-sm">
                 Don&apos;t have an account?{" "}
